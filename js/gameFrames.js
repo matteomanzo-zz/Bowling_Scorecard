e672e4b1050fc70f5212cb5b185deab2f8529ab6
matteomanzo <imatteo.manzo@gmail.com>
2015-01-30 11:50:51 +0000

    frame can receive rolls

--- js/gameFrames.js
+++ js/gameFrames.js
@@ -1,4 +1,14 @@
 var GameFrames = function() {
-  this.rolls = 2;
+  this.firstRollScore = 0;
+  this.secondRollScore = 0;
   this.pins = 10;
+};
+
+GameFrames.prototype.receiveFirstRoll = function(knockedDownPins) {
+  this.firstRollScore = knockedDownPins;
+  if (knockedDownPins === this.pins) {this.firstRollScore = 'Strike!'};
+};
+
+GameFrames.prototype.receiveSecondRoll = function(knockedDownPins) {
+  this.secondRollScore = (knockedDownPins + this.firstRollScore);
 };
--- spec/gameFramesSpec.js
+++ spec/gameFramesSpec.js
@@ -1,21 +1,44 @@
 describe('The first 9 game frames', function() {
 
-  var gameFrames;
+  var gameFrame;
 
   beforeEach(function() {
-    gameFrames = new GameFrames();
+    gameFrame = new GameFrames();
   });
 
   describe('by default', function() {
 
-    it('should have two rolls each', function() {
-      expect(gameFrames.rolls).toEqual(2);
-    });
-
     it('should contain 10 pins each', function() {
-      expect(gameFrames.pins).toEqual(10);
+      console.log(gameFrame)
+      expect(gameFrame.pins).toEqual(10);
     });
 
+    it('should have the first roll score equal to 0', function() {
+      expect(gameFrame.firstRollScore).toEqual(0);
+    });
+
+    it('should have the second roll score equal to 0', function() {
+      expect(gameFrame.secondRollScore).toEqual(0);
+    });
+  });
+
+  describe('when receive rolls', function() {
+
+    it('the first roll score should be the number of knocked down pins', function() {
+      gameFrame.receiveFirstRoll(4);
+      expect(gameFrame.firstRollScore).toEqual(4);
+    });
+
+    it('the first roll score should be a strike if 10 pins are knocked down', function() {
+      gameFrame.receiveFirstRoll(10);
+      expect(gameFrame.firstRollScore).toEqual('Strike!');
+    });
+
+    it('the second roll score should be the number of knocked down pins added to the first roll score', function() {
+      gameFrame.receiveFirstRoll(4);
+      gameFrame.receiveSecondRoll(5);
+      expect(gameFrame.totalScore).toEqual(9);
+    });
   });
 
 });
